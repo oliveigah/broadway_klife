d66a3652af13895a656f5aa123d184da64af4516
Gabriel Oliveira <oliveira.gahenrique@gmail.com>
2026-05-26 18:51:40 -0300

    mix new

---
+++ lib/broadway_klife.ex
@@ -0,0 +1,18 @@
+defmodule BroadwayKlife do
+  @moduledoc """
+  Documentation for `BroadwayKlife`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> BroadwayKlife.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/broadway_klife_test.exs
@@ -0,0 +1,8 @@
+defmodule BroadwayKlifeTest do
+  use ExUnit.Case
+  doctest BroadwayKlife
+
+  test "greets the world" do
+    assert BroadwayKlife.hello() == :world
+  end
+end
